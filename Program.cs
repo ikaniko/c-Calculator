11a1229eb0933b5980036300780ed8045b463334
Ika NIKO <ashotrustavelyan1@gmail.com>
2022-07-13 18:38:53 +0400

    fdkjgkdjfhgkdf

--- Program.cs
+++ Program.cs
@@ -31,7 +31,7 @@ namespace Calculator
 
             Console.WriteLine("suka bliau, idi naxui pidaras");
 
-            Console.WriteLine("pshel naxui ruski ia vashu mamu");
+            Console.WriteLine("pshel naxui ruski ia vashu mamu, rot ebal");
 
         }
     }
